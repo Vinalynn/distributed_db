4770e32711a5ef8406df06bf7edbc48049a01492
Vinalynn <william.cai@live.com>
2013-05-08 23:23:11 +0800

    spring + freemarker

--- src/main/java/org/vinalynn/jw/dist_db/web/controller/DistributedDBRequestController.java
+++ src/main/java/org/vinalynn/jw/dist_db/web/controller/DistributedDBRequestController.java
@@ -3,7 +3,9 @@
  */
 package org.vinalynn.jw.dist_db.web.controller;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -14,7 +16,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class DistributedDBRequestController {
 	
 	@RequestMapping(value="/sayhello.html")
-	public String sayHello() throws Exception{
-		return "Hello World!";
+	public String sayHello(Model model) throws Exception{
+		model.addAttribute("str", RandomStringUtils.random(20, Boolean.TRUE, Boolean.TRUE));
+		return "common/stringTemplate";
+	}
+	
+	@RequestMapping(value = "/rand_number.request")
+	public String getRandomNumber(Model model) throws Exception{
+		model.addAttribute("str", RandomStringUtils.random(20, Boolean.FALSE, Boolean.TRUE));
+		return "common/stringTemplate";
 	}
 }
